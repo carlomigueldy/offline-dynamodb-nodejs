4004ef1d154826e6078727ccf40df56a001ea967
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-03-11 14:20:48 +0800

    create table Movies script

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const AWS = require('aws-sdk')
 const uuidv4 = require('node-uuid')
-const dynamo = require('dynamodb')
 
-// console.log(uuidv4())
+AWS.config.update({
+	region: "us-west-2",
+	endpoint: "http://localhost:8000"
+})
 
-const Account = dynamo.define('Account', {
-	hashKey: 'email',
-	timestamps: true,
+const dynamodb = new AWS.DynamoDB()
 
-	schema: {
-		email: Joi.string().email(),
-		name: Joi.string(),
-		age: Joi.number(),
+var params = {
+	TableName : "Movies",
+	KeySchema: [       
+			{ AttributeName: "year", KeyType: "HASH"},  //Partition key
+			{ AttributeName: "title", KeyType: "RANGE" }  //Sort key
+	],
+	AttributeDefinitions: [       
+			{ AttributeName: "year", AttributeType: "N" },
+			{ AttributeName: "title", AttributeType: "S" }
+	],
+	ProvisionedThroughput: {       
+			ReadCapacityUnits: 10, 
+			WriteCapacityUnits: 10
+	}
+};
+
+dynamodb.createTable(params, (err, data) => {
+	if (err) {
+		console.error('Unable to create table. Error JSON:', JSON.stringify(err, null, 2))
+	} else {
+		console.log('Created table. Table description JSON:', JSON.stringify(data, null, 2))
 	}
 })
