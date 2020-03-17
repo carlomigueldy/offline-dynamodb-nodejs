fe4db28bdee023c25bfd78c1bc4eaa4c9ee81a0a
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-03-17 13:02:20 +0800

    added list tables cmd

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ AWS.config.update({
 
 const dynamodb = new AWS.DynamoDB()
 
-var params = {
+const params = {
 	TableName : "Movies",
 	KeySchema: [       
-			{ AttributeName: "year", KeyType: "HASH"},  //Partition key
-			{ AttributeName: "title", KeyType: "RANGE" }  //Sort key
+		{ AttributeName: "year", KeyType: "HASH"},  //Partition key
+		{ AttributeName: "title", KeyType: "RANGE" }  //Sort key
 	],
 	AttributeDefinitions: [       
-			{ AttributeName: "year", AttributeType: "N" },
-			{ AttributeName: "title", AttributeType: "S" }
+		{ AttributeName: "year", AttributeType: "N" },
+		{ AttributeName: "title", AttributeType: "S" }
 	],
 	ProvisionedThroughput: {       
-			ReadCapacityUnits: 10, 
-			WriteCapacityUnits: 10
+		ReadCapacityUnits: 10, 
+		WriteCapacityUnits: 10
 	}
 };
 
